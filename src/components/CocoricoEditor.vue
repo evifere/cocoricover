7f31dceb88e1f396b1143414b8c33f20c044f268
JOLLY <evifere@gmail.com>
2018-06-22 17:24:32 +0200

    add title zone

--- src/components/CocoricoEditor.vue
+++ src/components/CocoricoEditor.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="page">
-  	<div>
-   		<canvas id='background' width="500" height="700"></canvas>
-   		<img  width="500" height="700" ref="preview" src="" />
-   	</div>
+    <div>
+      <canvas id='background'></canvas>
+      <img  width="500" height="700" ref="preview" src="" />
+    </div>
    <div>
-   		<span>fontFamily : </span>
-		<select v-model="fontFamily">
-		  <option v-for="font in fonts" v-bind:value="font">
-		    {{ font }}
-		  </option>
-		</select>
-		<button   v-on:click="saveToPng" >Preview as png</button>
+      <span>fontFamily : </span>
+    <select v-model="fontFamily">
+      <option v-for="font in fonts" v-bind:value="font">
+        {{ font }}
+      </option>
+    </select>
+    Top Text
+    <input type="text" v-model="topText" />
+
+    <button   v-on:click="saveToPng" >Preview as png</button>
    </div>
   </div>
 </template>
@@ -21,79 +24,124 @@ import { fabric } from "fabric";
 let FontFaceObserver = require("fontfaceobserver");
 
 export default {
-  name: 'CocoricoEditor',
+  name: "CocoricoEditor",
   props: {
     msg: String
   },
 
-	data() {
-		return {
-			fontFamily: "",
-			fonts: ["Pacifico", "VT323", "Quicksand", "Inconsolata"]
-		};
-	},
+  data() {
+    return {
+      fontFamily: "",
+      topText: "Taupe texte izi year !",
+      titleText:"Le Ch'titre qui l'es bien là",
+      fonts: ["Pacifico", "VT323", "Quicksand", "Inconsolata"]
+    };
+  },
 
-	mounted() {
-		/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-		this.$canvas = new fabric.Canvas("background");
+  mounted() {
+    /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+    this.$canvas = new fabric.Canvas("background",{
+      width:500,
+      height:700
+    });
 
-		let textbox = new fabric.Textbox("Lorum ipsum dolor sit amet", {
-			left: 50,
-			top: 50,
-			width: 150,
-			fontSize: 20
-		});
-		this.$canvas.add(textbox).setActiveObject(textbox);
-		this.saveToPng();
-	},
+    //heading line
+    this.$canvas.add(this.makeLine([5, 0, 495, 0], "blue"));
 
-	methods: {
-		loadAndUse(font) {
-			let myfont = new FontFaceObserver(font);
-			let _self = this;
-			myfont
-				.load()
-				.then(function() {
-					// when font is loaded, use it.
-					_self.$canvas.getActiveObject().set("fontFamily", font);
-					_self.$canvas.requestRenderAll();
-				})
-				.catch(function(e) {
-					console.log(e);
-					alert("font loading failed " + font);
-				});
-		},
+    //top text
+    let topTextbox = new fabric.Textbox(this.topText, {
+      left: 50,
+      top: 10,
+      width: 400,
+      fontSize: 20,
+      fontStyle: "italic",
+      borderColor :'green',
+    });
 
-		saveToPng() {
-			this.$refs.preview.src = this.$canvas.toDataURL("image/png");
-		}
-	},
-	watch: {
-		fontFamily: function() {
-			/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-			console.log("fontFamily changed to " + this.fontFamily, this);
 
-			if (this.fontFamily !== "Times New Roman") {
-				this.loadAndUse(this.fontFamily);
-			} else {
-				this.$canvas
-					.getActiveObject()
-					.set("fontFamily", this.fontFamily);
-				this.$canvas.requestRenderAll();
-			}
-		}
-	}
+    this.$canvas.add(topTextbox).setActiveObject(topTextbox);
+
+    //top text
+    let titleTextbox = new fabric.Textbox(this.titleText, {
+      left: 10,
+      top: 410,
+      width: 490,
+      height: 2000,
+      textBackgroundColor :'blue',
+      borderColor :'green',
+      selectionBackgroundColor:'yellow',
+      backgroundColor:'blue',
+      stroke:'blue',
+      fill:"white",
+      fontSize: 60,
+      fontStyle: "normal"
+    });
+
+
+    this.$canvas.add(titleTextbox).setActiveObject(titleTextbox);
+
+
+
+    this.saveToPng();
+  },
+
+  methods: {
+    loadAndUse(font) {
+      let myfont = new FontFaceObserver(font);
+      let _self = this;
+      myfont
+        .load()
+        .then(function() {
+          // when font is loaded, use it.
+          _self.$canvas.getActiveObject().set("fontFamily", font);
+          _self.$canvas.requestRenderAll();
+        })
+        .catch(function(e) {
+          console.log(e);
+          alert("font loading failed " + font);
+        });
+    },
+
+    saveToPng() {
+      this.$refs.preview.src = this.$canvas.toDataURL("image/png");
+    },
+
+    makeLine(coords, color) {
+      return new fabric.Line(coords, {
+        fill: color,
+        stroke: color,
+        strokeWidth: 5,
+        selectable: false,
+        originX:'left',
+        originY:'top',
+        
+      });
+    }
+  },
+  watch: {
+    fontFamily: function() {
+      /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+      console.log("fontFamily changed to " + this.fontFamily, this);
+
+      if (this.fontFamily !== "Times New Roman") {
+        this.loadAndUse(this.fontFamily);
+      } else {
+        this.$canvas.getActiveObject().set("fontFamily", this.fontFamily);
+        this.$canvas.requestRenderAll();
+      }
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 div.page {
-	display: inline-flex;
+  display: inline-flex;
 }
 
 canvas,
 img {
-	border: 1px solid blue;
+  border: 1px solid blue;
 }
 </style>
