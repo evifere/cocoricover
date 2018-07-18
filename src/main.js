d6cdc4fb0a248f55dbfbb631a464597f95d976ab
JOLLY <evifere@gmail.com>
2018-07-18 09:22:41 +0200

    add locale fr element ui

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import '../theme/index.css'
 import '../theme/display.css'
 import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/fr'
 import App from './App.vue'
 Vue.config.productionTip = false
 
-Vue.use(Element);
+Vue.use(Element,{locale});
 
 new Vue({
   render: h => h(App)
